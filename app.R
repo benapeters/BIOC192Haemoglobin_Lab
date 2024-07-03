05af36cc43bc78e718bf8e5673e69a2e93e4dee6
Ben Peters <benjamin.peters@otago.ac.nz>
2024-07-04 10:55:13 +1200

    Updated app with new tab names.

--- app.R
+++ app.R
@@ -1,51 +1,46 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
+library(rhandsontable)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
+# Define UI
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  titlePanel("BIOC192 Lab 3"),
+  
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Exercise 1",
+                 rHandsontableOutput("table1")),  
+        tabPanel("Exercise 2",
+                 rHandsontableOutput("table2")),  
+      )
     )
-)
+  )
 
-# Define server logic required to draw a histogram
+
+# Define server logic
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  # Create initial data for tables
+  initial_data1 <- data.frame(
+    X = c(1, 2, 3),
+    Y = c(10, 20, 30)
+  )
+  
+  initial_data2 <- data.frame(
+    A = c("A", "B", "C"),
+    B = c(100, 200, 300)
+  )
+  
+  # Render tables
+  output$table1 <- renderRHandsontable({
+    rhandsontable(initial_data1)
+  })
+  
+  output$table2 <- renderRHandsontable({
+    rhandsontable(initial_data2)
+  })
+  
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+# Run the app
+shinyApp(ui, server)
